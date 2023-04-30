fa56eb9a3e8b993f11ef2266d3894f433c75f08f
bolon667 <46919302+bolon667@users.noreply.github.com>
2023-04-30 12:38:50 +0300

    pub

--- 16_PaletteRotation/src/main.c
+++ 16_PaletteRotation/src/main.c
@@ -18,38 +18,17 @@ void leftRotate(u16 arr[], u16 d, u16 n)
 
 void paletteRotationScene(){
 	
-	VDP_resetScreen();
-	SPR_init();
-	VDP_setPlaneSize(64,32,TRUE);
-	VDP_setScrollingMode(HSCROLL_PLANE, VSCROLL_PLANE);
-	VDP_setVerticalScroll(BG_B, 0);
-	VDP_setVerticalScroll(BG_A, 0);
-	VDP_setHorizontalScroll(BG_B, 0);
-	VDP_setHorizontalScroll(BG_A, 0);
-	
 	u16 my_pal0[16];
 	PAL_setPalette(PAL0, img.palette->data, CPU);
 
-	
 	curTileInd = 16;
-
-	s16 pal0_timer = 3;
-	
 	PAL_getPalette(PAL0, my_pal0);
 	SYS_doVBlankProcess();
 	VDP_drawImageEx(BG_B, &img, TILE_ATTR_FULL(PAL0, 0, FALSE, FALSE, curTileInd), 0, 0, FALSE, TRUE);
 	curTileInd += img.tileset->numTile;
 	while(1){
-		pal0_timer--;
 		leftRotatebyOne(my_pal0, 16);
 		PAL_setPalette(PAL0, my_pal0, DMA);
-		// if(pal0_timer < 0){
-		// 	pal0_timer = 3;
-		// 	PAL_fadeToPalette(PAL0, my_pal0, 3, TRUE);
-		// }
-		
-
-		SPR_update();
 		SYS_doVBlankProcess();
 	}
 }
